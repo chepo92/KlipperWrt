2d2bf831b9fe4273f51045f20bac663bca07706b
TheRealGramdalf <gramdalftech@gmail.com>
2023-04-01 11:20:51 -0700

    Use UCI in btrfs.sh

--- Scripts/btrfs.sh
+++ Scripts/btrfs.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Bind mounts
 DEVICE='sda1'
 MOUNTPOINT='/etc/klipper'
 # Name of the directory to mount under `/usr/lib/`. This may change between python versions, check the wiki if "Not enough space" errors are thrown
@@ -7,48 +6,83 @@ PYTHONNAME='python3.10'
 GCCNAME='gcc'
 KLIPPERNAME='klipper'
 # Mountpoint paths
-NGINX='/etc/nginx'
-PYTHON="/usr/lib/$PYTHONNAME"
-GCC="/usr/lib/$GCCNAME"
+NGINXDIR='/etc/nginx'
+PYTHONDIR="/usr/lib/$PYTHONNAME"
+GCCDIR="/usr/lib/$GCCNAME"
+
+mkfs.btrfs "$DEVICE" --label "KlipperWRT"
 # Evaluate `block info $DEVICE` to obtain the uuid as a variable
+# This works by evaluating any string matching `VAR=value` from stdin, and declaring it a variable
 eval "$(block info ${DEVICE} | grep -o -e "UUID=\S*")"
 
 # Create parent directory
 mkdir -p "$MOUNTPOINT"
+# Mount the device correctly
 mount "/dev/$DEVICE" "$MOUNTPOINT"
 
 # Create the gcc subvolume, make the bind mount directory
-btrfs subvol create "$MOUNTPOINT/$GCCNAME"
-mkdir -p "$GCC"
+btrfs subvolume create "$MOUNTPOINT/$GCCNAME"
+mkdir -p "$GCCDIR"
 
 
 # Create the python3.10 subvolume, make the bind mount directory
-btrfs subvol create "$MOUNTPOINT/$PYTHONNAME"
-mkdir -p "$PYTHON"
+btrfs subvolume create "$MOUNTPOINT/$PYTHONNAME"
+mkdir -p "$PYTHONDIR"
 
 
 # Create the nginx subvolume, make the bind mount directory
-btrfs subvol create "$MOUNTPOINT/$NGINXNAME"
-mkdir -p "$NGINX"
+btrfs subvolume create "$MOUNTPOINT/$NGINXNAME"
+mkdir -p "$NGINXDIR"
+# Copy the preexisting files from `/etc/nginx/*` to the btrfs subvolume
+cp -r '/etc/nginx' "$MOUNTPOINT/$NGINXNAME/*" 
 
 # Create the klipper subvolume
-btrfs subvol create "$MOUNTPOINT/$KLIPPERNAME"
+btrfs subvolume create "$MOUNTPOINT/$KLIPPERNAME"
 
+# Add an anonymous section, mount
+# This section is for the root volume
+uci add fstab mount
+# Add fstab options to the last anonymous section
+uci set fstab.@mount[-1].target="$MOUNTPOINT"
+uci set fstab.@mount[-1].fstype='btrfs'
+uci set fstab.@mount[-1].enabled='1'
+uci set fstab.@mount[-1].uuid="$UUID"
 
-# fstab.@mount[0]=mount
-# fstab.@mount[0].uuid='cf7da29e-73f6-4912-8b8e-0abb01e9a780'
-# fstab.@mount[0].target='/etc/klipper'
-# fstab.@mount[0].fstype='btrfs'
-# fstab.@mount[0].enabled='1'
-# fstab.@mount[1]=mount
-# fstab.@mount[1].target='/usr/lib/gcc'
-# fstab.@mount[1].fstype='btrfs'
-# fstab.@mount[1].options='subvol=/gcc'
-# fstab.@mount[1].uuid='cf7da29e-73f6-4912-8b8e-0abb01e9a780'
-# fstab.@mount[1].enabled='1'
-# fstab.@mount[2]=mount
-# fstab.@mount[2].target='/usr/lib/python3.10'
-# fstab.@mount[2].fstype='btrfs'
-# fstab.@mount[2].options='subvol=/python3.10'
-# fstab.@mount[2].enabled='1'
-# fstab.@mount[2].uuid='cf7da29e-73f6-4912-8b8e-0abb01e9a780'
+# Add an anonymous section, mount
+# This section is for the python installation
+uci add fstab mount
+# Add fstab options to the last anonymous section
+uci set fstab.@mount[-1].target="$PYTHONDIR"
+uci set fstab.@mount[-1].fstype='btrfs'
+uci set fstab.@mount[-1].options="subvol=$PYTHONNAME"
+uci set fstab.@mount[-1].enabled='1'
+uci set fstab.@mount[-1].uuid="$UUID"
+
+# Add an anonymous section, mount
+# This is for the gcc installation
+uci add fstab mount
+# Add fstab options to the last anonymous section
+uci set fstab.@mount[-1].target="$GCCDIR"
+uci set fstab.@mount[-1].fstype='btrfs'
+uci set fstab.@mount[-1].options="subvol=$GCCNAME"
+uci set fstab.@mount[-1].enabled='1'
+uci set fstab.@mount[-1].uuid="$UUID"
+
+# Add an anonymous section, mount
+# This is for the nginx files
+uci add fstab mount
+# Add fstab options to the last anonymous section
+uci set fstab.@mount[-1].target="$NGINXDIR"
+uci set fstab.@mount[-1].fstype='btrfs'
+uci set fstab.@mount[-1].options="subvol=$NGINXNAME"
+uci set fstab.@mount[-1].enabled='1'
+uci set fstab.@mount[-1].uuid="$UUID"
+
+uci commit fstab
+
+# Unmount the $DEVICE in case it is in use
+umount -q "$DEVICE"
+block umount
+block mount
+
+# TODO: Add a test that checks whether or not the bind mounts worked correctly, by creating a file in $MOUNTPOINT/$NGINXNAME checking that it exists in $NGINXDIR
